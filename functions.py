08a4a9d73c7f37ae21c31af2542233807db9d4df
Kathoni <thrusherkeys@gmail.com>
2026-02-21 07:31:30 +0300

    functions and methods

---
+++ functions.py
@@ -0,0 +1,13 @@
+first = [11, 12, 13, 14, 15]
+second = [16, 17, 18, 19, 20]
+third = ["Mom", "Dad", "Brother", "Sister"]
+full = first + second
+print(full)
+
+full_sorted = sorted(full , reverse=True)
+print(full_sorted)
+
+#Methods are functions that belong to an object like lists... 
+first.index(13)
+print(first.index(13))
+
